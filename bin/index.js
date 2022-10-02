57c789f41ae6ff080877ec1f5711362f88ce8c79
Nico Rithner <nicorithner@gmail.com>
2022-10-01 18:53:26 -0600

    Add handle arguments

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv));
 
-console.log( "Hello! In a little while I tell you about today" );
+let date = new Date();
+
+const options = argv
+  .default("d", true)
+  .usage("Needs an argument")
+  .option("d", {
+    alias: "date",
+    describe: "Today's date",
+    demandOption: false,
+  })
+  .usage("Usage: -n <name>")
+  .option("n", {
+    alias: "name",
+    describe: "Add your name",
+    type: "string",
+    demandOption: false,
+  }).argv;
+
+// output
+if (options.n) {
+  console.log(`Hello ${options.name}, today is ${date}`);
+} else {
+  console.log(`${date}`);
+}
